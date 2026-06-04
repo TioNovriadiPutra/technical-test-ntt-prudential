fa1884f18891859c880cf2e5dfb0bb3a0b1f9fb0
TioNovriadiPutra <tionvriadi@gmail.com>
2026-06-05 00:57:21 +0700

    feat: add home page

--- src/pages/app/Home.tsx
+++ src/pages/app/Home.tsx
@@ -1,5 +1,27 @@
+import { Flex } from "@/components/shared";
+import useUserService from "@/services/user.service";
+import { Ripples } from "ldrs/react";
+import "ldrs/react/Ripples.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { useGetUserInfoService } = useUserService();
+
+  const { finalData, isLoading } = useGetUserInfoService();
+
+  return (
+    <Flex className="flex-1 items-center justify-center">
+      \
+      {isLoading ? (
+        <Ripples size="70" speed="2" color="var(--neutral-800)" />
+      ) : (
+        <>
+          <h1>Welcome</h1>
+
+          <p>{finalData.fullName}</p>
+        </>
+      )}
+    </Flex>
+  );
 };
 
 export default Home;
